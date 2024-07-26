c3d5b00f77908372c18dd809db480b6784b60ec5
Erica Ford <erica.ford0205@gmail.com>
2024-07-25 20:17:33 -0500

    Create the starting method, a test string and dictionary

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+dictionary = ["No", "body"]
+
+str = "His name was nobody"
+
+# Method checks dictionary(array) for substring occurences of the string argument
+# returns hash, key => dict(substring) value => occurences
+def substrings (str, dictionary)
+  
+end
