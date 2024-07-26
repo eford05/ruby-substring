cdb6535d87fc4fcce9ca38535265013b710e2c9e
Erica Ford <erica.ford0205@gmail.com>
2024-07-26 11:30:02 -0500

    Start writing logic to check the hash for key from dictionary

--- substrings.rb
+++ substrings.rb
@@ -5,11 +5,24 @@ str = "His name was nobody"
 # Method checks dictionary(array) for substring occurences of the string argument
 # returns hash, key => dict(substring) value => occurences
 def substrings (str, dictionary)
+  # Return a hash
+  hash_result = {}
+
   # Iterate over the dictionary
   dictionary.each {|i| 
-  # Test if string includes dictionary value at index
-  puts str.downcase.include? (i.downcase)
+  # Includes the dictionary value true else false
+  includes_substring = str.downcase.include? (i.downcase)
+
+  # If the string does include the substring
+  if includes_substring
+    # Check if hash result contains the value as a key if not add to the hash with occurences as value
+    puts hash_result.key?(i)
+
+  end
 }
+
+return hash_result
+
 end
 
 # Test
