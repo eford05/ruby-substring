ca56c4a66e7b1ff36e9b531a9f84145b05861165
Erica Ford <erica.ford0205@gmail.com>
2024-07-26 10:32:53 -0500

    Begin code inside method, iterate over dictionary array, test string include? method

--- substrings.rb
+++ substrings.rb
@@ -5,5 +5,12 @@ str = "His name was nobody"
 # Method checks dictionary(array) for substring occurences of the string argument
 # returns hash, key => dict(substring) value => occurences
 def substrings (str, dictionary)
-  
+  # Iterate over the dictionary
+  dictionary.each {|i| 
+  # Test if string includes dictionary value at index
+  puts str.downcase.include? (i.downcase)
+}
 end
+
+# Test
+substrings(str, dictionary)
